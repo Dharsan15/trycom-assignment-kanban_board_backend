d60d1edb1d9fd6527070aaccc848ec99847682d1
Dharsan15 <dharsanravi6677@gmail.com>
2025-02-09 20:59:22 +0530

    updated frontend cors

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ app.use(cors({
 
   app.use((req, res, next) => {
 
-    res.header("Access-Control-Allow-Origin", "https://trycom-assignment-kanban-board-kvca-pi.vercel.app/");
+    res.header("Access-Control-Allow-Origin", "https://trycom-assignment-kanban-board-kvca-pi.vercel.app");
     res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   
